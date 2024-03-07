abab604aac97bc20e71d78f50131331de9efb11e
bepes-code <75368713+bepes-code@users.noreply.github.com>
2024-03-07 17:42:54 +0100

    1:33

---
+++ trello/app/(platform)/layout.tsx
@@ -0,0 +1,18 @@
+import { ClerkProvider } from "@clerk/nextjs";
+
+const PlatfromLayout = ({
+    children
+}: {
+    children: React.ReactNode;
+}) => {
+
+    return (
+        <ClerkProvider>
+            {children}
+        </ClerkProvider>
+    )
+
+
+}
+
+export default PlatfromLayout;
